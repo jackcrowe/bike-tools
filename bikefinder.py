44889dd4e4c405af55979c08b0ff9e131b4231fe
Duane (Jack) Crowe <duane.r.crowe@gmail.com>
2014-03-15 20:09:30 -0500

    Added documentation and globals for bikesizecalculator

--- bikefinder.py
+++ bikefinder.py
@@ -1,30 +1,34 @@
-# This script opens a simple GUI that allows a user to find the proper bike size for different sorts of bikes
-# using the cyclist's measurements and preferences.
+""" This script opens a simple GUI that allows a user to find the proper bike size for different sorts of bikes
+    using the cyclist's measurements and preferences."""
+
 from Tkinter import *
 import ttk
 import bikesizecalculator
 
-    
+# create root window object    
 root = Tk()
-root.title("Feet to Meters")
+root.title("Bike Finder")
 
+# The types of bicycles that can be searched for, and a dictionary to store whether
+# each type of bike was selected
 bicycle_types = ["Touring", "Commuter", "Track", "Road", "Mixte"]
-
 number_of_types = len(bicycle_types)
 bicycle_types_selected = dict(zip(bicycle_types, [0]* number_of_types))
 
+# Creates a main window using ttk, with appropriate grid and padding
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
+# variable that stores the person's height in cm
 person_height = IntVar()
 
-
-
+# dynamically creates checkbox buttons for bike types
 current_column = 2
 current_row = 2
 for bicycle_type in bicycle_types:
+    # for each bike type, create a checkbox and move to the next row
     indicator = StringVar()
     bicycle_types_selected[bicycle_type] = indicator
     ttk.Checkbutton(mainframe, text=bicycle_type, variable=indicator).grid(
@@ -33,13 +37,16 @@ for bicycle_type in bicycle_types:
     print bicycle_types_selected[bicycle_type]
     current_row += 1
 
+# Create a slider to select height in centimeters
 slider = Scale(mainframe, label="Slider", from_=20, to=100, orient=HORIZONTAL, tickinterval=5, length=600, variable = person_height)
 slider.grid(column=current_column, row=current_row, sticky=E)
 current_row += 1
 
+# for each of the object created, do magic grid operation
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
 
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-root.bind('<Return>', bikesizecalculator.generate_craigslist_query(bicycle_types, person_height) )
-
+findbutton = ttk.Button(mainframe, text='Find Bikes!', command=bikesizecalculator.generate_craigslist_query(bicycle_types, person_height))
+findbutton.grid(column=3, row=current_row, sticky=E)
+    
 root.mainloop()
--- bikesizecalculator.py
+++ bikesizecalculator.py
@@ -1,20 +1,41 @@
 """
 bikesizecalculator:: a module for calculating the bike size appropriate for a person.
-
-Copyright (c) 2014 Duane R. Crowe. All Rights Reserved.
 """
 
-
+# Uses the craigslist python library from github
+# https://github.com/jackcrowe/bike-tools.git
 from craigslist import *
 
+# globals to store categorization of bike types
+mountain_geometry = "MTN"
+road_geometry = "ROAD"
+stepthrough_geometry = "STEP"
+
+# dictionary for bike type to geometry categorization
+bike_type_categories = {
+     'Touring' : road_geometry,
+     'Commuter' : road_geometry,
+     'Track' : road_geometry,
+     'Road' : road_geometry,
+     'Mixte' : stepthrough_geometry,
+     'Hardtail' : mountain_geometry,
+     'XC' : mountain_geometry }
+
+""" calculates the correct bike size for the given bike type and person's height"""
 def calculate_bike_size(bike_type, person_height):
     try:
-          
-        value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+        category = get_geometry_categorization(bike_type)
+        if category == road_geometry:
+            return 22
+        return 0
     except ValueError:
         pass
       
+""" generates a craigslist given an array of bike types and a person's height"""
 def generate_craigslist_query(bike_types, person_height):
-        search(' http://portland.craigslist.org/', 'bia', 'Schwinn', 'T', 'sss')
-        
+    search('http://portland.craigslist.org/', 'bia', 'Schwinn', 'T', 'sss')
+    return
+
+""" looks up the category of geometry for a bike type """
+def get_geometry_categorization(bike_type):
+    return bike_type_categories[bike_type]
